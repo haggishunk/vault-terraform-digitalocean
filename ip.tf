5d719cbd28a1b1d72a2008be9eb873b978962a2e
haggishunk <travis@mattera.io>
2020-09-21 20:50:09 -0700

    Using floating ip assignment

--- ip.tf
+++ ip.tf
@@ -1,4 +1,8 @@
 resource "digitalocean_floating_ip" "vault" {
+  region = digitalocean_droplet.vault.region
+}
+
+resource "digitalocean_floating_ip_assignment" "vault" {
+  ip_address = digitalocean_floating_ip.vault.ip_address
   droplet_id = digitalocean_droplet.vault.id
-  region     = digitalocean_droplet.vault.region
 }
